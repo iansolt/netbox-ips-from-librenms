2271c599c626731583bcd863689e23d256f9e4ce
iansolt <67619230+iansolt@users.noreply.github.com>
2024-06-15 11:06:34 -0400

    Update config-example.py

--- config-example.py
+++ config-example.py
@@ -1,6 +1,6 @@
-NETBOX_TOKEN = ''
-LIBRE_TOKEN = ''
-NETBOX_URL = ''
-LIBRE_URL = ''
-VERIFY_SSL = False
+NETBOX_TOKEN = '153540136603538'
+LIBRE_TOKEN = '789971942261988'
+NETBOX_URL = 'https://netbox.example.local/'
+LIBRE_URL = 'https://librenms.example.local/'
+VERIFY_SSL = True
 LOG_FILE = 'netbox-lnms-sync.log'
