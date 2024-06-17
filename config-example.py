82e0f098f1a606fb50cdc950da620872afda9dea
iansolt <67619230+iansolt@users.noreply.github.com>
2024-06-17 14:25:23 -0400

    Update config-example.py
    Clarified absolute path of LOG_FILE

--- config-example.py
+++ config-example.py
@@ -3,4 +3,4 @@ LIBRE_TOKEN = '789971942261988'
 NETBOX_URL = 'https://netbox.example.local/'
 LIBRE_URL = 'https://librenms.example.local/'
 VERIFY_SSL = True
-LOG_FILE = 'netbox-lnms-sync.log'
+LOG_FILE = '~/netbox-lnms-sync/netbox-lnms-sync.log'
